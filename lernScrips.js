154236d59fe418cae64cab5d7fd5b5cf43e393ec
Rustam <murdosilov@gmail.com>
2022-09-20 21:00:28 +0500

    debug

--- lernScrips.js
+++ lernScrips.js
@@ -1,20 +1,23 @@
-"use strict";
+  "use strict";
 const options = {
-    name: 'test',
-    widht: 1024,
-    leight: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    }
-  };
+     name: 'test',
+     widht: 1024,
+     leight: 1024,
+     colors: {
+         border: 'black',
+         bg: 'red'
+     }
+   };
 
   for (let key in options) {
-      if (typeof(options[key]) === "object") {
+       if (typeof(options[key]) === "object") {
           for (let i in options[key]) {
-                console.log(`Свойство ${i} имеет значение ${options[key] [i]}`);
-          }
-          } else {
-          console.log(`Свойство ${key} имеет значение ${options[key]}`);
-      }
-    }
+                 console.log(`Свойство ${i} имеет значение ${options[key] [i]}`);
+           }
+           } else {
+           console.log(`Свойство ${key} имеет значение ${options[key]}`);
+       }
+     }
+
+
+    
