7414a5994c702d0241154ed4c5625acb1ed851aa
Rustam <murdosilov@gmail.com>
2022-09-20 23:25:08 +0500

    20/09/22

--- lernScrips.js
+++ lernScrips.js
@@ -19,5 +19,23 @@ const options = {
        }
      }
 
+function getMathResult(num, times) {
+  if (typeof (times) !== 'number' || times <= 0) {
+    return num;
+  }
 
-    
+  let str = '';
+
+  for (let i = 1; i <= times; i++) {
+    if (i === times) {
+      str += `${num * i}`;
+      // Тут без черточек в конце
+    } else {
+      str += `${num * i}---`;
+      // Это тоже самое, что и
+      // str = str + num * i + "---"
+    }
+  }
+
+  return str;
+}
