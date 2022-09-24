e862691e941267c0c608af60fe4c61116ddff047
Rustam <murdosilov@gmail.com>
2022-09-25 00:34:21 +0500

    new day

--- lernScrips.js
+++ lernScrips.js
@@ -1,41 +1,23 @@
-  "use strict";
-const options = {
-     name: 'test',
-     widht: 1024,
-     leight: 1024,
-     colors: {
-         border: 'black',
-         bg: 'red'
-     }
-   };
-
-  for (let key in options) {
-       if (typeof(options[key]) === "object") {
-          for (let i in options[key]) {
-                 console.log(`Свойство ${i} имеет значение ${options[key] [i]}`);
-           }
-           } else {
-           console.log(`Свойство ${key} имеет значение ${options[key]}`);
-       }
-     }
-
-function getMathResult(num, times) {
-  if (typeof (times) !== 'number' || times <= 0) {
-    return num;
-  }
-
-  let str = '';
-
-  for (let i = 1; i <= times; i++) {
-    if (i === times) {
-      str += `${num * i}`;
-      // Тут без черточек в конце
-    } else {
-      str += `${num * i}---`;
-      // Это тоже самое, что и
-      // str = str + num * i + "---"
-    }
-  }
-
-  return str;
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+for (let i = 0; i < arr.length; i++) {
+  result[i] = arr[i];
+  
 }
+console.log(result);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+for (let i = 0; i < data.length; i++) {
+  if (typeof(data[i]) === 'number') {
+      data[i] = data[i] * 2;
+  } else if (typeof(data[i]) === 'string') {
+      data[i] = `${data[i]} - 'done'`;
+  }
+console.log(data);
+}
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+for (let i = 1; i <= data.length; i++) {
+  result[i-1] = data[data.length - i]
+}
+  console.log(result);
